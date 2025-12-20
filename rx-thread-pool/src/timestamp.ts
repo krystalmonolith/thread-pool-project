bdbd38a5e79eee79db0dd62c20adc24cb24dc6ec
Mark Deazley <mdeazley@gmail.com>
2025-12-20 09:49:03 -0500

    Fixed maxDepth at 2, CSV output file timestamp.

---
+++ rx-thread-pool/src/timestamp.ts
@@ -0,0 +1,8 @@
+export const ts = (date: Date = new Date()): string =>
+    date.getFullYear() +
+    String(date.getMonth() + 1).padStart(2, '0') +
+    String(date.getDate()).padStart(2, '0') +
+    'T' +
+    String(date.getHours()).padStart(2, '0') +
+    String(date.getMinutes()).padStart(2, '0') +
+    String(date.getSeconds()).padStart(2, '0');
